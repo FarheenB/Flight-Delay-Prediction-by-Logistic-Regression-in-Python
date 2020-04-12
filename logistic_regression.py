2fe59ef0120ac089db8feb7adda41e9425dc4ff9
FarheenB <farheenbano94@gmail.com>
2020-04-13 00:28:57 +0530

    Updated codes

--- logistic_regression.py
+++ logistic_regression.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from numpy.linalg import inv
 
 
@@ -25,12 +25,24 @@ flights_data = pd.read_csv('dataset/FlightDelays.csv')
 # In[3]:
 
 
+flights_data.head()
+
+
+# In[4]:
+
+
+flights_data=flights_data.rename(columns={"Flight_Status": "FLIGHT_STATUS"})
+
+
+# In[5]:
+
+
 # Check if there are any null values in the dataframe
 flights_data.isnull().any()
 # There are no null values in the data as shown below
 
 
-# In[4]:
+# In[6]:
 
 
 #Check for null values in the dataset
@@ -38,14 +50,33 @@ sns.heatmap(flights_data.isnull(), yticklabels=False)
 plt.show()
 
 
-# In[5]:
+# In[7]:
 
 
 # Cross check the data types of all the columns before modeling
 print(flights_data.dtypes)
 
 
-# In[6]:
+# In[8]:
+
+
+flights_data.head()
+
+
+# In[9]:
+
+
+len(flights_data)
+
+
+# In[10]:
+
+
+flights_data.columns
+len(flights_data.columns)
+
+
+# In[11]:
 
 
 #converting the object type(String type) to categorical data
@@ -55,7 +86,7 @@ from collections import defaultdict
 d = defaultdict(LabelEncoder)
 
 #selecting cols that need to be transformed
-df = pd.DataFrame(flights_data, columns = ['CARRIER', 'DEST', 'FL_DATE', 'ORIGIN','TAIL_NUM','Flight_Status'])
+df = pd.DataFrame(flights_data, columns = ['CARRIER', 'DEST', 'FL_DATE', 'ORIGIN','TAIL_NUM','FLIGHT_STATUS'])
 
 # Encoding the variable
 fit = df.apply(lambda x: d[x.name].fit_transform(x))
@@ -64,7 +95,16 @@ fit = df.apply(lambda x: d[x.name].fit_transform(x))
 fit.apply(lambda x: d[x.name].inverse_transform(x))
 
 # Using the dictionary to label future data
-flights_df=pd.DataFrame(df.apply(lambda x: d[x.name].transform(x)))
+flights_cat=pd.DataFrame(df.apply(lambda x: d[x.name].transform(x)))
+
+# creating instance of one-hot-encoder
+enc = OneHotEncoder(handle_unknown='ignore')
+enc_df = pd.DataFrame(enc.fit_transform(flights_cat[['CARRIER', 'DEST', 'FL_DATE', 'ORIGIN','TAIL_NUM']]).toarray())
+flights_df =enc_df
+# flights_df
+
+
+
 
 #add the rest of the cols to the dataframe
 flights_df['CRS_DEP_TIME']=flights_data['CRS_DEP_TIME']
@@ -74,30 +114,32 @@ flights_df['FL_NUM']=flights_data['FL_NUM']
 flights_df['Weather']=flights_data['Weather']
 flights_df['DAY_WEEK']=flights_data['DAY_WEEK']
 flights_df['DAY_OF_MONTH']=flights_data['DAY_OF_MONTH']
+flights_df['FLIGHT_STATUS']=flights_cat['FLIGHT_STATUS']
 
 #print top 5 values of the dataset
+# flights_df.head()
 flights_df.head()
 
 
-# In[7]:
+# In[12]:
 
 
 #select dependent and independent variables
 
-X = flights_df.drop({'Flight_Status'}, axis=1)
-y = flights_df['Flight_Status']
-
-
-# In[8]:
-
-
-#split dataset into train and test data by 60:40 ratio
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=12)
+X = flights_df.drop({'FLIGHT_STATUS'}, axis=1)
+y = flights_df['FLIGHT_STATUS']
 
 
 # # Logistic Regression
 
-# In[9]:
+# In[13]:
+
+
+#split dataset into train and test data by 60:40 ratio
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=4)
+
+
+# In[14]:
 
 
 class LogisticRegression:
@@ -143,54 +185,41 @@ class LogisticRegression:
     
 
 
-# In[10]:
+# In[15]:
 
 
 #call Logistic Regression class
-regressor = LogisticRegression(learning_rate=0.0001, n_iters=1000)
+regressor = LogisticRegression(learning_rate=0.001, n_iters=1000)
 regressor.fit(X_train, y_train)
 y_pred = regressor.predict(X_test)
 
 
-# In[11]:
+# In[16]:
+
+
+y_pred
+
+
+# In[17]:
 
 
 #find accuracy of the model
 def accuracy(y_true, y_pred):
-        accuracy = np.sum(y_true == y_pred) / len(y_true)
-        return accuracy
+    accuracy = np.sum(y_true == y_pred) / len(y_true)
+    return accuracy
 
 
-# In[12]:
+# In[18]:
 
 
 #accuracy of the model
 print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(accuracy(y_test, y_pred)))
 
 
-# # Finding Coefficients
-
-# In[13]:
+# In[19]:
 
 
-# Changing the index of the records to sequential
-X_train.index=range(len(X_train))
-y_train.index=range(len(y_train))
-
-# Convert X and Y train sets to matrices
-X=np.array(X_train)
-Y=np.array(y_train)
-
-# Calculate coefficients from (XT.X)-1.XT.Y
-coefficients=inv(X.transpose().dot(X)).dot(X.transpose()).dot(Y)
-# Coefficients from the equation
-print(coefficients)
-
-
-# In[14]:
-
-
-#logistic Regression from Scikit learn (uncomment to model report for logistic regression by scikit learn)
+#logistic Regression from Skit learn (uncomment for scikit )
 # from sklearn.linear_model import LogisticRegression
 # classifier = LogisticRegression(max_iter=1000,random_state = 12)
 # classifier.fit(X_train, y_train)
@@ -200,14 +229,14 @@ print(coefficients)
 
 # # Model Report
 
-# In[15]:
+# In[20]:
 
 
 from sklearn.metrics import accuracy_score
 print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(accuracy_score(y_test, y_pred)))
 
 
-# In[16]:
+# In[21]:
 
 
 from sklearn.metrics import confusion_matrix
@@ -215,7 +244,7 @@ tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
 print("Confusion Matrix:", tn, " ", fp, " ", fn, " ", tp)
 
 
-# In[17]:
+# In[22]:
 
 
 # Sensitivity
@@ -223,7 +252,7 @@ sensitivity=tp/(fn+tp)
 print('Sensitivity: {:.2f}'.format(sensitivity))
 
 
-# In[18]:
+# In[23]:
 
 
 # Specificity
@@ -231,23 +260,23 @@ specificity=tn/(tn+fp)
 print('Specificity: {:.2f}'.format(specificity))
 
 
-# In[19]:
+# In[24]:
 
 
 # Precision
 precision=tp/(tp+fp)
-print('Precision: {:.2f}'.format(precision))
+print('precision: {:.2f}'.format(precision))
 
 
-# In[20]:
+# In[25]:
 
 
 # Accuracy and misclassfication rate
 misclassification=1-(accuracy_score(y_test, y_pred))
-print('Misclassification: {:.2f}'.format(misclassification))
+print('misclassification: {:.2f}'.format(misclassification))
 
 
-# In[21]:
+# In[26]:
 
 
 from sklearn.metrics import classification_report
